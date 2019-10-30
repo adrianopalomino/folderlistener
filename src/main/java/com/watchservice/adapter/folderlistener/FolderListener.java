4805e496e1b42f62cefec3a46a990252001aa9bf
= <adrianoplmn66@gmail.com>
2019-10-30 02:59:38 -0200

    refactory

---
+++ src/main/java/com/watchservice/adapter/folderlistener/FolderListener.java
@@ -0,0 +1,39 @@
+package com.watchservice.adapter.folderlistener;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+/**
+ * @author adrianopalomino
+ *
+ */
+public class FolderListener {
+
+	public static void listen() {
+		// LISTENER "C:\\Users\\pc\\Desktop"
+		try {
+			Path path = Paths.get("C:\\Users\\pc\\Desktop");
+			WatchService watchService = path.getFileSystem().newWatchService();
+			path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY,
+					StandardWatchEventKinds.ENTRY_DELETE);
+			while (true) {
+				WatchKey watchKey = watchService.take();
+				for (WatchEvent<?> event : watchKey.pollEvents()) {
+					System.out.println(event);
+					System.out.println("printou");
+				}
+				if (!watchKey.reset()) {
+					watchKey.cancel();
+					watchService.close();
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
