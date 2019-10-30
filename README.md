cc590f914fbecd8ce193d205cdccdf8816fcf445
= <adrianoplmn66@gmail.com>
2019-10-30 02:50:21 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# folderlistener
+
+mvn spring-boot:run
+
+ou execute a classe:
+
+com.spring.base.FolderListenerApplication
---
+++ src/test/java/com/watchservice/adapter/folderlistener/FolderlistenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchservice.adapter.folderlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolderlistenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
